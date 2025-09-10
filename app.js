41c8b44527bc1321c0d5a3854bf06b5d7bf81a7b
Maria Mair <mm225mz@student.lnu.se>
2025-09-10 22:55:10 +0200

    Add main file with function outline

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * The main starting point of the module.
+ *
+ * @author Maria Mair <mm225mz@student.lnu.se>
+ * @version 0.0.1
+ */
+
+// Get data input
+
+// Get number of intervals
+
+// Get color scheme
+
+// Check type of data input
+
+// Sort data
+
+// Group data into intervals
+
+// Add color from selected scheme to each interval
+
+// Return data as interval with color scheme
