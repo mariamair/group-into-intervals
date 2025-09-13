63eb272d857f64a6facec45f9d9ec6c5936e09f0
Maria Mair <mm225mz@student.lnu.se>
2025-09-13 11:09:34 +0200

    Add ColorSelector

---
+++ ColorSelector.js
@@ -0,0 +1,38 @@
+/** 
+ * This class contains methods to match colors to the data input.
+ * 
+ * @author Maria Mair <mm225mz@student.lnu.se>
+ * @version 0.0.1
+ */
+
+export class ColorSelector {
+
+  selectColorScheme (colorSchemeId) {
+    const colorSchemes = [ 
+      { 
+        id: 1, name: 'red, violet, blue', 
+        hexValues: ['#be2020', '#7532a8', '#1a02f0'], 
+        rgbValues: ['rgb(190, 32, 32)', 'rgb(117, 50, 168)', 'rgb(26, 2, 240)'] 
+      }, {
+        id: 2, name: 'red, yellow, blue', 
+        hexValues: ['#be2020', '#d6db42', '#1a02f0'], 
+        rgbValues: ['rgb(190, 32, 32)', 'rgb(214, 219, 66)', 'rgb(26, 2, 240)'] 
+      }, {
+        id: 3, name: 'violet, yellow, blue',
+        hexValues: ['#7532a8', '#d6db42', '#1a02f0'], 
+        rgbValues: ['rgb(117, 50, 168)', 'rgb(214, 219, 66)', 'rgb(26, 2, 240)'] 
+      }, {
+        id: 4, name: 'white, light green, dark green',
+        hexValues: ['#fff', '#5ec138', '#3a6d25'], 
+        rgbValues: ['rgb(255, 255, 255)', 'rgb(94, 193, 56)', 'rgb(58, 109, 37)'] 
+    }]
+
+    for (const scheme of colorSchemes) {
+      if (colorSchemeId === scheme.id) {
+        return scheme
+      }
+    }
+
+    throw Error ('Invalid color scheme')
+  }
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  */
 
 import { DataProcessingService } from './DataProcessingService.js'
+import { ColorSelector } from './ColorSelector.js'
 
 // Get data input
 const dataArray = [1, 3, -5, 7, 8, 4, 17, 12, 3]
@@ -13,11 +14,16 @@ const shortArray = [1, 3, -5, 17, 3]
 const noArray = 13
 const sparseArray = [1, 3, -5, , 8, 4, 17, 12, 3]
 const mixedArray = [1, 17, 'test', 3, -5, 17, 3]
+const textArray = ['Apple', 'Banana', 'Maracuja', 'Lemon', 'Banana']
 
 // Get sort order (ascending or descending)
 const isAscending = true
 
 // Get color scheme
+const selectedColorScheme = 1
+const colorSelector = new ColorSelector()
+console.log('Selected color scheme: ' + colorSelector.selectColorScheme(selectedColorScheme).name)
+console.log(colorSelector.selectColorScheme(selectedColorScheme))
 
 // Add color from selected scheme to each interval
 
@@ -28,6 +34,7 @@ try {
   console.log(dataService.defineIntervals())
   console.log('original data input: ' + dataArray)
   
+  console.log('\nSHORT ARRAY')
   const dataServiceShortArray = new DataProcessingService(shortArray, isAscending)
   console.log('\ndata grouped in intervals 2 \n--------------------------- ')
   console.log(dataServiceShortArray.defineIntervals())
@@ -38,6 +45,7 @@ try {
   console.log(dataServiceDesc.defineIntervals())
   console.log('original data input: ' + dataArray)
   
+  console.log('\nSHORT ARRAY DESC')
   const dataServiceShortArrayDesc = new DataProcessingService(shortArray, false)
   console.log('\ndata grouped in intervals 4 \n--------------------------- ')
   console.log(dataServiceShortArrayDesc.defineIntervals())
@@ -47,13 +55,22 @@ try {
   console.log('\ndata grouped in intervals 5 \n--------------------------- ')
   console.log(dataServiceNoArray.defineIntervals()) */
   
-/*   const dataServiceSparseArray = new DataProcessingService(sparseArray, false)
+  console.log('\nSPARSE ARRAY')
+  const dataServiceSparseArray = new DataProcessingService(sparseArray, false)
   console.log('\ndata grouped in intervals 6 \n--------------------------- ')
-  console.log(dataServiceSparseArray.defineIntervals()) */
+  console.log(dataServiceSparseArray.defineIntervals())
+  console.log('original data input: ' + sparseArray)
   
+/*   console.log('MIXED ARRAY')
   const dataServiceMixedArray = new DataProcessingService(mixedArray, false)
   console.log('\ndata grouped in intervals 7 \n--------------------------- ')
-  console.log(dataServiceMixedArray.defineIntervals())
+  console.log(dataServiceMixedArray.defineIntervals()) */
+  
+/*   console.log('\nTEXT ARRAY')
+  const dataServiceTextrray = new DataProcessingService(textArray, false)
+  console.log('\ndata grouped in intervals 7 \n--------------------------- ')
+  console.log(dataServiceTextrray.defineIntervals())
+  console.log('original data input: ' + textArray) */
 } catch (error) {
   console.error(error)
 }
