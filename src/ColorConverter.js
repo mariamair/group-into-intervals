ef843ab01d806ec3d551e39b95f82fa55a1bc67a
Maria Mair <mm225mz@student.lnu.se>
2025-09-16 20:25:51 +0200

    Place converting functions in ColorConverter class

---
+++ src/ColorConverter.js
@@ -0,0 +1,24 @@
+/** 
+ * This class contains methods to convert between hexadecimal and rgb values.
+ * 
+ * @author Maria Mair <mm225mz@student.lnu.se>
+ * @version 0.0.1
+ */
+
+export class ColorConverter {
+
+  convertRgbStringToArray (rgbValueString) {
+    const separator = /[(),]/
+    const convertToInt = (element) => Number.parseInt(element)
+
+    return rgbValueString.split(separator).filter(convertToInt).map(convertToInt)
+  }
+
+  convertRgbArrayToString (rgbValueArray) {
+    return 'rgb(' + rgbValueArray[0] + ', ' + rgbValueArray[1] + ', ' + rgbValueArray[2] + ')'
+  }
+
+  convertRgbArraytoHexValue (rgbValueArray) {
+    return '#' + rgbValueArray[0].toString(16) + rgbValueArray[1].toString(16) + rgbValueArray[2].toString(16)
+  }
+}
