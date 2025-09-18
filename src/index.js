5dde01796f3d08c1e8ea2b128d619ad5d8bd3bf7
Maria Mair <mm225mz@student.lnu.se>
2025-09-18 14:10:55 +0200

    Improve groupIntoIntervalsWithOptions function and add new function getIntervalMetadata

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * @version 0.0.1
  */
 
-import { DataProcessingService } from './IntervalCreator.js'
+import { IntervalCreator } from './IntervalCreator.js'
 import { ColorSelector } from './ColorSelector.js'
 import { IntervalAndColorMatcher } from './IntervalAndColorMatcher.js'
 
@@ -15,24 +15,29 @@ export function displayColorSchemes () {
 }
 
 export function groupIntoIntervalsAscending (data) {
-  const intervalCreator = new DataProcessingService(data)
+  const intervalCreator = new IntervalCreator(data)
   return intervalCreator.getIntervals()
 }
 
 export function groupIntoIntervalsDescending (data) {
-  const intervalCreator = new DataProcessingService(data, false)
+  const intervalCreator = new IntervalCreator(data, false)
   return intervalCreator.getIntervals()
 }
 
-export function groupIntoIntervalsWithOptions (data, isAscending, selectedColorScheme) {
-  const intervalCreator = new DataProcessingService(data, isAscending)
+export function groupIntoIntervalsWithOptions (data, selectedColorScheme, isAscending) {
+  // Check if selectedColorScheme is within range 1 - 4 and that isAscending is boolean (if it is provided)
+  const intervalCreator = new IntervalCreator(data, isAscending)
   const intervals = intervalCreator.getIntervals()
-  const numberOfIntervals = intervalCreator.getIntervalMetadata().numberOfIntervals
   
-  const colorMatcher = new IntervalAndColorMatcher(selectedColorScheme, intervals, numberOfIntervals)
+  const colorMatcher = new IntervalAndColorMatcher(intervals, selectedColorScheme)
   const intervalsWithColors = colorMatcher.getColors()
 
   return intervalsWithColors
+}
+
+export function getIntervalMetadata (data, isAscending) {
+  const intervalCreator = new IntervalCreator(data, isAscending)
+  return intervalCreator.getIntervalMetadata()
 }
 
 
