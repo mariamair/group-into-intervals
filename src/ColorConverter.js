b9024ff7c4485a79fe0b39f813c512b4d23c300a
Maria Mair <mm225mz@student.lnu.se>
2025-09-23 12:24:24 +0200

    Add JSDoc comments and update version number

--- src/ColorConverter.js
+++ src/ColorConverter.js
@@ -1,12 +1,18 @@
 /** 
- * This class contains methods to convert between hexadecimal and rgb values.
+ * This class contains methods to convert between hexadecimal and rgb color values.
  * 
  * @author Maria Mair <mm225mz@student.lnu.se>
- * @version 0.0.1
+ * @version 1.0.0
  */
 
 export class ColorConverter {
 
+  /**
+   * Convert an RGB string to an array of RGB values.
+   * 
+   * @param {string} rgbValueString - An RGB string specifying a color.
+   * @returns {number[]} - An array with numbers representing the RGB values of a color.
+   */
   convertRgbStringToArray (rgbValueString) {
     const separator = /[(),]/
     const convertToInt = (element) => Number.parseInt(element)
@@ -14,19 +20,38 @@ export class ColorConverter {
     return rgbValueString.split(separator).filter(convertToInt).map(convertToInt)
   }
 
+  /**
+   * Convert multiple RGB strings to a nested array of RGB values.
+   * 
+   * @param {string[]} rgbStringWithMultipleValues - An array containing multiple RGB value strings.
+   * @returns {number{}} - An array containing arrays with numbers representing RGB values.
+   */
   convertMultipleRgbStringsToArray (rgbStringWithMultipleValues) {
     const rgbArray = []
 
     for(const color of rgbStringWithMultipleValues) {
       rgbArray.push(this.convertRgbStringToArray(color))
     }
+
     return rgbArray
   }
 
+  /**
+   * Convert an array of RGB values into a color string with RGB notation.
+   *
+   * @param {number[]} rgbValueArray - Array with numbers representing RGB values.
+   * @returns {string} - An RGB string specifying a color.
+   */
   convertRgbArrayToString (rgbValueArray) {
     return 'rgb(' + rgbValueArray[0] + ', ' + rgbValueArray[1] + ', ' + rgbValueArray[2] + ')'
   }
 
+  /**
+   * Convert an array of RGB values into a color string with hexadecimal notation.
+   * 
+   * @param {number[]} rgbValueArray - Array with numbers representing RGB values.
+   * @returns {string} - A hexadecimal string specifying a color.
+   */
   convertRgbArraytoHexValue (rgbValueArray) {
     return '#' + rgbValueArray[0].toString(16).padStart(2, '0') + rgbValueArray[1].toString(16).padStart(2, '0') + rgbValueArray[2].toString(16).padStart(2, '0')
   }
