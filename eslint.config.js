0243a882baa0c8bcee0c844f740d8145fafc240e
Maria Mair <mm225mz@student.lnu.se>
2025-09-27 18:16:01 +0200

    Configure stylistic rules for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,27 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+  { 
+    files: ['**/*.{js,mjs,cjs}'], 
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      'brace-style': ['error', '1tbs'],
+      camelcase: ['error', { properties: 'always' }],
+      'eol-last': ['error', 'always'],
+      indent: ['error', 2],
+      'key-spacing': ['error', { 'mode': 'strict' }],
+      'max-depth': ['warn', { 'max': 3 }],
+      'max-len': ['warn', 
+        { 
+          'code': 80,
+          'ignoreComments': true,
+          'ignoreUrls': true 
+        }],
+      'max-params': ['warn', { 'max': 3 }],
+      'object-curly-spacing': ['error', 'always'],
+      quotes: ['error', 'single']
+    }
+  }
+])
